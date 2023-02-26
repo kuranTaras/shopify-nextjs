f6bfb7dd6ee02a43c808e625b974cc7fb20700cc
kuranTaras <rebystaras@gmail.com>
2023-02-26 21:56:25 +0100

    added basic SEO

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -4,6 +4,7 @@ import ProductForm from "./ProductForm";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import {Navigation, Pagination, A11y} from "swiper";
 import 'swiper/swiper-bundle.css'
+import RecommendedList from "./RecommendedList";
 
 const ProductPageContent = ({product}) => {
 
@@ -25,22 +26,28 @@ const ProductPageContent = ({product}) => {
 
 
     return (
-        <div className={'flex flex-col justify-center items-center -spacy-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto'}>
-            <div className={'w-full max-w-md border bg-white rounded-2xl overflow-hidden shadow-lg md:w-1/2'}>
-                <div className={'relative h-96 w-full '}>
-                    <Swiper
-                        modules={[Navigation, Pagination, A11y]}
-                        navigation
-                        pagination={{ clickable: true }}
-                        style={{'--swiper-navigation-color': '#000', '--swiper-pagination-color': '#000' }}
-                        className={'h-96 rounded-2xl'}
-                        loop={'true'}
-                    >
-                        {images}
-                    </Swiper>
+        <div>
+            <div className={'flex flex-col justify-center items-center -spacy-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto'}>
+                <div className={'w-full max-w-md border bg-white rounded-2xl overflow-hidden shadow-lg md:w-1/2'}>
+                    <div className={'relative h-96 w-full '}>
+                        <Swiper
+                            modules={[Navigation, Pagination, A11y]}
+                            navigation
+                            pagination={{ clickable: true }}
+                            style={{'--swiper-navigation-color': '#000', '--swiper-pagination-color': '#000' }}
+                            className={'h-96 rounded-2xl'}
+                            loop={'true'}
+                        >
+                            {images}
+                        </Swiper>
+                    </div>
                 </div>
+                <ProductForm product={product} />
             </div>
-            <ProductForm product={product} />
+            <p className={'pt-16 space-y-8 md:space-x-4 lg:space-x-8 max-w-3xl w-11/12 mx-auto'}>
+                {product.description}
+            </p>
+            <RecommendedList products={product.collections.edges[0].node.products.edges} current={product.id} />
         </div>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
 import {getProductsInCollection} from "../../lib/shopify";
 import ProductList from "../../components/ProductList";
+import Hero from "../../components/Hero";
+import Head from 'next/head'
 
 export default function Home({products}) {
 
-  return (
-      <h1 className="text-xl">
-        <ProductList products={products} />
-      </h1>
-  )
+    return (
+        <div>
+            <Head>
+                <title>
+                    Modern eCommerce Course
+                </title>
+                <meta http-equiv={'Content-Type'} content={'text/html; charset=utf-8'} />
+                <meta http-equiv={'Content-Type'} content={'text/html; charset=ISO-8859-1'} />
+                <meta name={'description'} content={'Modern eCommerce Development Course focusing on Shopify, Next.js, TailwindCSS, GraphQL. Additional topics include Storefront API, Static Site Generation, getStaticPaths, getStaticProps and more.'} />
+                <meta property="og:title" content=" Modern eCommerce Course" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://shopify-nextjs-g7i5.vercel.app" />
+                <meta property="og:image" content="https://shopify-nextjs-g7i5.vercel.app/share.png" />
+                <meta property="og:description"
+                      content="Modern eCommerce Development Course focusing on Shopify, Next.js, TailwindCSS, GraphQL. Additional topics include Storefront API, Static Site Generation, getStaticPaths, getStaticProps and more." />
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:site_name" content="Modern eCommerce Course" />
+            </Head>
+            <Hero />
+            <ProductList products={products} />
+        </div>
+    )
 }
 
 
